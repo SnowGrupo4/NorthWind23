07d89a3b22fe3822491415a54bf776563cbac791
SnowGrupo4 <130581715+SnowGrupo4@users.noreply.github.com>
2023-04-14 00:55:44 -0600

    Add files via upload

---
+++ fin_test/target/compiled/fin_test/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from Northwind.nw.my_first_dbt_model
+where id is null
+
+
---
+++ fin_test/target/compiled/fin_test/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from Northwind.nw.my_second_dbt_model
+where id is null
+
+
